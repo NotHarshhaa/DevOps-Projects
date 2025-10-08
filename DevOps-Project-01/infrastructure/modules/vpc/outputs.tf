754648d79bca92799be876526bdc8b07eae7acac
Hitesh <mehiteshmondal@gmail.com>
2025-10-08 20:38:43 +0530

    Fixed VPC module issues infrastructure 	modified:   DevOps-Project-01/infrastructure/modules/vpc/main.tf 	modified:   DevOps-Project-01/infrastructure/modules/vpc/outputs.tf 	modified:   DevOps-Project-01/infrastructure/modules/vpc/variables.tf

--- DevOps-Project-01/infrastructure/modules/vpc/outputs.tf
+++ DevOps-Project-01/infrastructure/modules/vpc/outputs.tf
@@ -1,8 +1,20 @@
+# Essential VPC outputs
 output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main.id
 }
 
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "vpc_name" {
+  description = "Name of the VPC from tags"
+  value       = lookup(aws_vpc.main.tags, "Name", null)
+}
+
+# Subnets
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
   value       = aws_subnet.public[*].id
@@ -13,12 +25,40 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "public_subnet_cidrs" {
+  description = "CIDR blocks of public subnets"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "private_subnet_cidrs" {
+  description = "CIDR blocks of private subnets"
+  value       = aws_subnet.private[*].cidr_block
+}
+
+# NAT Gateways
 output "nat_gateway_ids" {
   description = "List of NAT Gateway IDs"
   value       = aws_nat_gateway.main[*].id
 }
 
-output "vpc_cidr_block" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
-} 
+output "nat_gateway_elastic_ips" {
+  description = "Elastic IPs associated with NAT Gateways"
+  value       = aws_eip.nat[*].public_ip
+}
+
+# Internet Gateway
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway attached to the VPC"
+  value       = aws_internet_gateway.main.id
+}
+
+# Route Tables
+output "public_route_table_ids" {
+  description = "List of public route table IDs"
+  value       = aws_route_table.public[*].id
+}
+
+output "private_route_table_ids" {
+  description = "List of private route table IDs"
+  value       = aws_route_table.private[*].id
+}
